5833c66c3286ba281209e7a6815f9912575b2578
sarkonovich <arkonovs@reed.edu>
2016-06-17 15:19:47 -0700

    Verify Request

---
+++ verify.rb
@@ -0,0 +1,60 @@
+require 'sinatra/base'
+
+module Sinatra 
+	module AlexaVerify
+
+	  def valid_address?
+	    valid_address = /^https:\/\/s3.amazonaws.com(:443)?\/echo.api\/.*?$/
+	    @url == @url.match(valid_address)[0] rescue false
+	  end
+
+	  def valid_timestamp?
+	    Time.now < DateTime.parse(@echo_request.timestamp).to_time + 150 rescue false
+	  end
+
+	  def valid_certificate?(certificate)
+	    certificate.subject.to_a.last.include?("echo-api.amazon.com") && 
+	    Time.now.utc > certificate.not_before && 
+	    Time.now.utc < certificate.not_after
+	  end
+
+	  def get_certificate(url)
+	  	begin
+  			OpenSSL::X509::Certificate.new HTTParty.get(url)
+  		rescue TypeError
+  			halt 400, "Bad Request"
+  		rescue OpenSSL::SSL::SSLError
+  			halt 400, "Bad Request"
+  		end
+  	end
+
+	  def check_signature(certificate)
+      certificate.public_key.verify(@digest, Base64.decode64(@signature), @data) rescue false
+    end
+
+    def verify_request
+
+	    if valid_address? && valid_timestamp?
+	    	@certificate = get_certificate(@url)
+	    else
+	    	halt 400, "Bad Request"
+	    end
+
+	    if valid_certificate?(@certificate)
+	    	@verify = check_signature(@certificate)
+	    else
+	    	halt 400, "Invalid Certificate"
+	    end
+
+	    if @verify
+	    	status 200
+	    else
+	    	halt 400, "Invalid Signature"
+	    end
+	  end
+	end
+ helpers AlexaVerify
+end
+
+
+
